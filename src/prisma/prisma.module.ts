5379f1cf4724a8e430d891b64bb5ac29f6e2e704
Luis Arce <luis.arce22@gmail.com>
2025-05-29 22:19:01 -0400

    Update prisma module and service to use generated schema

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,7 +1,7 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
-@Global() // Make PrismaService available globally
+@Global()
 @Module({
   providers: [PrismaService],
   exports: [PrismaService],
